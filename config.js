fffcdec989966dc8f40d6265aea1e0f473675faa
fengjia0912 <123814030@qq.com>
2025-11-20 15:27:23 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// 数据库配置文件
+// 注意：请将此文件添加到.gitignore中，不要提交到GitHub仓库
+
+window.supabaseConfig = {
+    SUPABASE_URL: 'https://txukpcoeliodbkgfnpgk.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4dWtwY29lbGlvZGJrZ2ZucGdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NDAzNzEsImV4cCI6MjA3OTAxNjM3MX0.9gL9t8on0jnoXZdRTMcWw42n_W5FOiXn-rxtjmhTmtQ'
+};
